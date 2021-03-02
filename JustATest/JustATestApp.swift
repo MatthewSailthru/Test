a68927ce7f1276071347fa12fb17b47b305f60cc
Matthew Tripodi <mtripodi@sailthru.com>
2021-03-02 16:56:07 -0500

    Initial Commit

---
+++ JustATest/JustATestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustATestApp.swift
+//  JustATest
+//
+//  Created by Matthew Tripodi on 3/2/21.
+//
+
+import SwiftUI
+
+@main
+struct JustATestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
